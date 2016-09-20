0ae6aea6024a071413adcc953a7d9d9f8f988d72
ANDREA <andrea.palomino.ruby@gmail.com>
2016-09-19 19:48:28 -0700

    arreglo de contador a negativo

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
-function contar() { 
-         var max = "150"; 
-         var cadena = document.getElementById("caracteres").value; 
-         var longitud = cadena.length; 
 
-             if(longitud <= max) { 
-                  document.getElementById("contador").value = max-longitud; 
-             } else { 
-                  document.getElementById("caracteres").value = cadena.substr(0, max); 
-             } 
-}
 window.addEventListener("load", function() {
-	var boton = document.getElementById("boton");
-	boton.addEventListener("click", function() {
+    var boton = document.getElementById("boton");
+    boton.disabled = true;
+    boton.addEventListener("click", function(e) {
+         e.preventDefault();
+         var textoArea =document.getElementById("caracteres");
+         var texto = document.getElementById("caracteres").value; 
+         crear();
 
-		 var cadena = document.getElementById("caracteres").value;
+	    function crear(){	
 		 var parrafo = document.createElement("div");
 		 var padre = document.getElementById("seccion");
-		 parrafo.innerHTML = cadena ;
-		 padre.appendChild(parrafo);
-		 padre.insertBefore(parrafo,padre.childNodes[0]);
+		     parrafo.innerHTML = texto ;
+		     padre.appendChild(parrafo);
+		     parrafo.classList.toggle("divis");
+		     padre.insertBefore(parrafo,padre.childNodes[0]);
+		     textoArea.value= "";
+    	      boton.disabled=true;  
+         }
+    });
+    caracteres.addEventListener("keypress", function() {
+        boton.disabled = false;
+        contar();
+        
 	});
+
+     function contar(){
+        var maximo = 140;
+        var textValue = document.getElementById("caracteres").value;
+        var largo = textValue.length;
+        var contador =  document.getElementById("contador");
+        contador.innerHTML = maximo - largo;
+         
+        if(largo > maximo) { 
+              contador.classList.add("red");
+              boton.disabled = true; 
+        } else { 
+              contador.classList.remove("red");
+        } 
+     };
+
 });
-
-
     
+   
